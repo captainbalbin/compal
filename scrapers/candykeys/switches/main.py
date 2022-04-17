0019c0d36c69ff4be432a5ca019ecbd1e14713bd
Jakub Dudek <jakub.a.dudek@gmail.com>
2022-04-18 00:07:25 +0200

    Introduce Switch data scraper for CandyKeys
    Scraper first gathers all URL for the available switches.
    It then iteras over each URL scraping the switch data and formating it into a JSON object.
    The objects are then printed it a res.json file

---
+++ scrapers/candykeys/switches/main.py
@@ -0,0 +1,115 @@
+from posixpath import sep
+import requests
+from bs4 import BeautifulSoup
+import json
+import re
+
+
+def tableContent(table):
+    row_name = []
+    row_data = []
+    trs = table.find_all('tr')
+    headerow = [td.get_text(strip=True)
+                for td in trs[0].find_all('th')]  # header row
+    if headerow:  # if there is a header reemove
+        trs = trs[1:]
+    for tr in trs:  # for every table row
+        name = tr.find_all('th')[0].get_text(strip=True)
+        data = tr.find_all('td')[0].get_text(strip=True)
+
+        row_name.append(name)
+        row_data.append(data)
+    return (row_name, row_data)
+
+
+class Switch:
+    def __init__(self,
+                 url='',
+                 name='',
+                 brand='',
+                 price='',
+                 feedback='',
+                 weight='',
+                 travel='',
+                 lubrication=''):
+        self.url = url
+        self.name = name
+        self.brand = brand
+        self.price = price
+        self.feedback = feedback
+        self.weight = weight
+        self.travel = travel
+        self.lubrication = lubrication
+
+
+#####################################################################################
+
+# Class soup.find_all("h1", class_="title m-b-2")
+# Attribute soup.find_all(attrs={":category": '["switches"]'})
+
+r = requests.get("https://candykeys.com/category:switches")
+soup = BeautifulSoup(r.text, 'html.parser')
+items = soup.find_all(attrs={":category": '["switches"]'})
+
+output = []
+
+for idx, item in enumerate(items):
+    print(f'{idx}/{len(items)}')
+    switch_url = item.get("url")
+    try:
+        r = requests.get(switch_url)
+        soup = BeautifulSoup(r.text, 'html.parser')
+
+        name = soup.find("h1", class_="title m-b-2").getText()
+
+        price = soup.find("span", class_="price").getText()
+        price = re.search('([0-9,]+).*€', price).group(1)
+
+        table = soup.find("div", class_="bx bg-lighter").table
+        (tableNames, tableRows) = tableContent(table)
+
+        try:
+            brand = tableRows[tableNames.index("Brand")]
+        except:
+            print(f'No brand at url: {switch_url}')
+
+        try:
+            feedback = tableRows[tableNames.index("Feedback")]
+        except:
+            print(f'No feedback at url: {switch_url}')
+
+        try:
+            weight = tableRows[tableNames.index("Spring Weight")]
+        except:
+            print(f'No weight at url: {switch_url}')
+
+        try:
+            travel = tableRows[tableNames.index("Travel")]
+        except:
+            print(f'No travel at url: {switch_url}')
+
+        try:
+            lubrication = tableRows[tableNames.index("Lubrication")]
+        except:
+            print(f'No lubrication at url: {switch_url}')
+
+        sw = Switch(
+            switch_url,
+            name,
+            brand,
+            price,
+            feedback,
+            weight,
+            travel,
+            lubrication
+        )
+
+        output.append(json.dumps(sw.__dict__))
+    except:
+        print(f'Failed at url: {switch_url}')
+
+f = open("res.json", "a")
+f.write("[")
+f.write(",".join(output))
+f.write("]")
+f.close()

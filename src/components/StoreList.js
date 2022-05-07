58951111ea1ec63e383623d1de42227606756585
captainbalbin <albin@sjostrand.dev>
2022-05-07 13:04:59 +0200

    Fix sorting of stores

--- src/components/StoreList.js
+++ src/components/StoreList.js
@@ -1,7 +1,7 @@
 import StoreListItem from './StoreListItem'
 
 const StoreList = ({ entity }) => {
-  const sortedStores = entity.stores.slice().sort((a, b) => (a.price > b.price ? 1 : -2))
+  const sortedStores = entity.stores.slice().sort((a, b) => (a.price > b.price ? 1 : -1))
 
   return (
     <div className="row-span-3 w-full max-w-7xl grid overflow-hidden gap-4 p-4">
